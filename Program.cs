f4a3697c0cb584fa04fd11a0013d138a6c54a2a0
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2025-09-09 15:08:23 +0200

    .

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Vad heter du?");
+        string input = Console.ReadLine();
+        
+        Console.WriteLine(input + " är ett väldigt vackert namn.");
     }
 }
